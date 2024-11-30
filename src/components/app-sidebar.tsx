57209cd73d71d866d7cf5946355217e2c4e2eaf5
M. Mousa Naeem <122889315+muhammad-mousa@users.noreply.github.com>
2024-11-30 17:31:29 +0500

    1 setup main layout for the authenticated users of application (#5)
    * configure shadcn in the project
    * setup main layout
    
    ---------
    
    Co-authored-by: Zubair Jamil <92372994+ZU3AIRE@users.noreply.github.com>

---
+++ src/components/app-sidebar.tsx
@@ -0,0 +1,116 @@
+"use client"
+
+import * as React from "react"
+import {
+  BookOpen,
+  Bot,
+  CalendarCheck,
+  Command,
+  Frame,
+  LifeBuoy,
+  Map,
+  MonitorCog,
+  PieChart,
+  Send,
+  Settings2,
+  SquareTerminal,
+} from "lucide-react"
+
+import { NavMain } from "@/components/nav-main"
+import { NavProjects } from "@/components/nav-projects"
+import { NavSecondary } from "@/components/nav-secondary"
+import { NavUser } from "@/components/nav-user"
+import {
+  Sidebar,
+  SidebarContent,
+  SidebarFooter,
+  SidebarHeader,
+  SidebarMenu,
+  SidebarMenuButton,
+  SidebarMenuItem,
+} from "@/components/ui/sidebar"
+
+const data = {
+  user: {
+    name: "shadcn",
+    email: "m@example.com",
+    avatar: "/avatars/shadcn.jpg",
+  },
+  navMain: [
+    {
+      title: "Attendence",
+      url: "#",
+      icon: CalendarCheck,
+      isActive: true,
+      items: [
+        {
+          title: "Mark Attendence",
+          url: "#",
+        },
+        {
+          title: "Export/Share",
+          url: "#",
+        },
+      ],
+    },
+    {
+      title: "System Management",
+      url: "#",
+      icon: MonitorCog,
+      items: [
+        {
+          title: "Courses",
+          url: "#",
+        },
+        {
+          title: "Students",
+          url: "#",
+        },
+      ],
+    },
+
+  ],
+  navSecondary: [
+    {
+      title: "Support",
+      url: "#",
+      icon: LifeBuoy,
+    },
+    {
+      title: "Feedback",
+      url: "#",
+      icon: Send,
+    },
+  ],
+}
+
+export function AppSidebar({ ...props }: React.ComponentProps<typeof Sidebar>) {
+  return (
+    <Sidebar variant="inset" {...props}>
+      <SidebarHeader>
+        <SidebarMenu>
+          <SidebarMenuItem>
+            <SidebarMenuButton size="lg" asChild>
+              <a href="#">
+                <div className="flex aspect-square size-8 items-center justify-center rounded-lg bg-sidebar-primary text-sidebar-primary-foreground">
+                  <Command className="size-4" />
+                </div>
+                <div className="grid flex-1 text-left text-sm leading-tight">
+                  <span className="truncate font-semibold">Mark Up Inc</span>
+                  <span className="truncate text-xs">Enterprise</span>
+                </div>
+              </a>
+            </SidebarMenuButton>
+          </SidebarMenuItem>
+        </SidebarMenu>
+      </SidebarHeader>
+      <SidebarContent>
+        <NavMain items={data.navMain} />
+        <NavSecondary items={data.navSecondary} className="mt-auto" />
+      </SidebarContent>
+      <SidebarFooter>
+        <NavUser user={data.user} />
+      </SidebarFooter>
+    </Sidebar>
+  )
+}

eadd3eebd8334fe6e5e20dfb8be80454e2d192fa
Zubair Jamil <xubairjamil@gmail.com>
2024-12-07 22:29:53 +0500

    Revert the course branch

--- src/app/courses/page.tsx
+++ src/app/courses/page.tsx
@@ -4,28 +4,42 @@ import { Checkbox } from "@/components/ui/checkbox"
 import { DropdownMenu, DropdownMenuCheckboxItem, DropdownMenuContent, DropdownMenuItem, DropdownMenuLabel, DropdownMenuSeparator, DropdownMenuTrigger } from "@/components/ui/dropdown-menu"
 import { Input } from "@/components/ui/input"
 import { Table, TableBody, TableCell, TableHead, TableHeader, TableRow } from "@/components/ui/table"
-import { ColumnDef, ColumnFiltersState, flexRender, getCoreRowModel, getFilteredRowModel, getPaginationRowModel, getSortedRowModel, SortingState, useReactTable, VisibilityState } from "@tanstack/react-table"
+import { Column, ColumnDef, ColumnFiltersState, flexRender, getCoreRowModel, getFilteredRowModel, getPaginationRowModel, getSortedRowModel, SortingState, useReactTable, VisibilityState } from "@tanstack/react-table"
 import { ArrowUpDown, ChevronDown, MoreHorizontal } from "lucide-react"
 import React from "react"
-
-import {
-    Dialog,
-    DialogContent,
-    DialogDescription,
-    DialogFooter,
-    DialogHeader,
-    DialogTitle,
-    DialogTrigger,
-} from "@/components/ui/dialog"
-import { Label } from "@/components/ui/label"
-
+import SmartSelect, { SmartSelectItem } from "@/components/re-useables/SmartSelect/page"
 
 const data: Course[] = [
-    { id: "m5gr84i9", title: "Data Structures & Algorithms", teacher: "John Doe", courseCode: "DI-325" },
-    { id: "3u1reuv4", title: "Programming Fundamentals", teacher: "Jane Doe", courseCode: "CC-123" },
-    { id: "derv1ws0", title: "Web Development", teacher: "Orion Pax", courseCode: "WD-456" },
-    { id: "5kma53ae", title: "Machine Learning", teacher: "Jeason", courseCode: "ML-789" },
-    { id: "bhqecj4p", title: "Software Engineering", teacher: " Ciliona", courseCode: "SE-101" },
+    {
+        id: "m5gr84i9",
+        title: "Data Structures & Algorithms",
+        teacher: "John Doe",
+        courseCode: "DI-325",
+    },
+    {
+        id: "3u1reuv4",
+        title: "Programming Fundamentals",
+        teacher: "Jane Doe",
+        courseCode: "CC-123",
+    },
+    {
+        id: "derv1ws0",
+        title: "Web Development",
+        teacher: "Orion Pax",
+        courseCode: "WD-456",
+    },
+    {
+        id: "5kma53ae",
+        title: "Machine Learning",
+        teacher: "Jeason",
+        courseCode: "ML-789",
+    },
+    {
+        id: "bhqecj4p",
+        title: "Software Engineering",
+        teacher: " Ciliona",
+        courseCode: "SE-101",
+    },
 ]
 
 export type Course = {
@@ -107,10 +121,10 @@ export const columns: ColumnDef<Course>[] = [
             return (
                 <DropdownMenu>
                     <DropdownMenuTrigger asChild>
-                        <div className="h-8 w-8 p-0 flex items-center justify-center">
+                        <Button variant="ghost" className="h-8 w-8 p-0">
                             <span className="sr-only">Open menu</span>
                             <MoreHorizontal />
-                        </div>
+                        </Button>
                     </DropdownMenuTrigger>
                     <DropdownMenuContent align="end">
                         <DropdownMenuLabel>Actions</DropdownMenuLabel>
@@ -120,24 +134,11 @@ export const columns: ColumnDef<Course>[] = [
                             Copy Course ID
                         </DropdownMenuItem>
                         <DropdownMenuSeparator />
-                        <DropdownMenuItem>
-                            <Dialog>
-                                <DialogTrigger>View Details</DialogTrigger>
-                                <DialogContent>
-                                    <DialogHeader>
-                                        <DialogTitle>{row.getValue("courseCode")}</DialogTitle>
-                                        <DialogDescription>
-                                            {row.getValue("courseCode")}
-                                        </DialogDescription>
-                                    </DialogHeader>
-                                </DialogContent>
-                            </Dialog>
-                        </DropdownMenuItem>
+                        <DropdownMenuItem>View Course</DropdownMenuItem>
                         <DropdownMenuItem>View Course details</DropdownMenuItem>
                     </DropdownMenuContent>
                 </DropdownMenu>
-            );
-
+            )
         },
     },
 ]
@@ -173,35 +174,7 @@ export default function Courses() {
         <div className="pe-4 ps-8">
             <div className="flex items-center justify-between py-4">
                 <h1 className="text-2xl font-semibold">Courses</h1>
-                <Button variant="outline"> <Dialog>
-                    <DialogTrigger>Add Course</DialogTrigger>
-                    <DialogContent className="sm:max-w-[625px]">
-                        <DialogHeader>
-                            <DialogTitle>New Course</DialogTitle>
-                            <DialogDescription>
-                                Add & assign course to semester.
-                            </DialogDescription>
-                        </DialogHeader>
-                        <div className="grid gap-4 py-4">
-                            <div className="grid grid-cols-4 items-center gap-4">
-                                <Label htmlFor="courseTitle" className="text-right">
-                                    Course Title
-                                </Label>
-                                <Input id="courseTitle" placeholder="Course name here" className="col-span-3" />
-                            </div>
-                            <div className="grid grid-cols-4 items-center gap-4">
-                                <Label htmlFor="courseCode" className="text-right">
-                                    Course Code
-                                </Label>
-                                <Input id="courseCode" placeholder="DC-878" className="col-span-3" />
-                            </div>
-                        </div>
-                        <DialogFooter>
-                            <Button type="submit">Save changes</Button>
-                        </DialogFooter>
-                    </DialogContent>
-                </Dialog>
-                </Button>
+                <Button variant="outline">Add Course</Button>
             </div>
             <div className="flex items-center py-4">
                 <Input
@@ -212,32 +185,29 @@ export default function Courses() {
                     }
                     className="max-w-sm"
                 />
-                <DropdownMenu>
-                    <DropdownMenuTrigger asChild>
-                        <Button variant="outline" className="ml-auto">
-                            Columns <ChevronDown />
-                        </Button>
-                    </DropdownMenuTrigger>
-                    <DropdownMenuContent align="end">
-                        {table
-                            .getAllColumns()
-                            .filter((column) => column.getCanHide())
-                            .map((column) => {
-                                return (
-                                    <DropdownMenuCheckboxItem
-                                        key={column.id}
-                                        className="capitalize"
-                                        checked={column.getIsVisible()}
-                                        onCheckedChange={(value) =>
-                                            column.toggleVisibility(!!value)
-                                        }
-                                    >
-                                        {column.id}
-                                    </DropdownMenuCheckboxItem>
-                                )
-                            })}
-                    </DropdownMenuContent>
-                </DropdownMenu>
+                <div className="ml-auto">
+                    <SmartSelect
+                        items={
+                            table
+                                .getAllColumns()
+                                .filter((column) => column.getCanHide())
+                                .map((column: Column<Course>) => {
+                                    return {
+                                        key: column.id,
+                                        label: column.id,
+                                        isChecked: column.getIsVisible()
+                                    }
+                                })}
+                        onCheckedChange={(item, value) =>
+                            table
+                                .getAllColumns()
+                                .filter((column) => column.getCanHide()).find(coulmn => coulmn.id == item.key)?.toggleVisibility(!!value)
+                        }
+                        title="Columns"
+                        variant={'outline'}
+                        key={'id'}
+                    ></SmartSelect>
+                </div>
             </div>
             <div className="rounded-md border">
                 <Table>
@@ -313,12 +283,7 @@ export default function Courses() {
                     </Button>
                 </div>
             </div>
-
-
         </div>
 
-
-
     )
-}
-
+}   

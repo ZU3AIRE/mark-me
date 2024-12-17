8caa806a08037374f98a6227cb887b17571f3957
M. Mousa Naeem <122889315+muhammad-mousa@users.noreply.github.com>
2024-12-17 22:30:41 +0500

    26 add new functionality for viewing and updating student in students (#40)
    * remove duplicate import from courses (#38) (#39)
    
    * #26 add student edit functionality
    
    * adjust the size of the dialog

--- src/app/students/page.tsx
+++ src/app/students/page.tsx
@@ -3,174 +3,16 @@ import SmartSelect from "@/components/re-useables/SmartSelect/page"
 import { Button } from "@/components/ui/button"
 import { Checkbox } from "@/components/ui/checkbox"
 import { Dialog, DialogContent, DialogDescription, DialogHeader, DialogTitle } from "@/components/ui/dialog"
-import { DropdownMenu, DropdownMenuCheckboxItem, DropdownMenuContent, DropdownMenuItem, DropdownMenuLabel, DropdownMenuSeparator, DropdownMenuTrigger } from "@/components/ui/dropdown-menu"
+import { DropdownMenu, DropdownMenuContent, DropdownMenuItem, DropdownMenuLabel, DropdownMenuSeparator, DropdownMenuTrigger } from "@/components/ui/dropdown-menu"
 import { Input } from "@/components/ui/input"
 import { Table, TableBody, TableCell, TableHead, TableHeader, TableRow } from "@/components/ui/table"
 import { Column, ColumnDef, ColumnFiltersState, flexRender, getCoreRowModel, getFilteredRowModel, getPaginationRowModel, getSortedRowModel, SortingState, useReactTable, VisibilityState } from "@tanstack/react-table"
-import { ArrowUpDown, ChevronDown, MoreHorizontal } from "lucide-react"
+import { ArrowUpDown, MoreHorizontal, SquarePen, Trash2 } from "lucide-react"
 import React, { useEffect, useState } from "react"
+import { toast } from "sonner"
 import { IStudent } from "../model/student"
 import { RegisterStudent } from "./add-student"
-import { useRouter } from "next/navigation"
-import { toast } from "sonner"
-
-export const columns: ColumnDef<IStudent>[] = [
-    {
-        id: "select",
-        header: ({ table }) => (
-            <Checkbox
-                checked={
-                    table.getIsAllPageRowsSelected() ||
-                    (table.getIsSomePageRowsSelected() && "indeterminate")
-                }
-                onCheckedChange={(value) => table.toggleAllPageRowsSelected(!!value)}
-                aria-label="Select all"
-            />
-        ),
-        cell: ({ row }) => (
-            <Checkbox
-                checked={row.getIsSelected()}
-                onCheckedChange={(value) => row.toggleSelected(!!value)}
-                aria-label="Select row"
-            />
-        ),
-        enableSorting: false,
-        enableHiding: false,
-    },
-    {
-        accessorKey: "collegeRollNo",
-        header: ({ column }) => {
-            return (
-                <Button
-                    variant="ghost"
-                    onClick={() => column.toggleSorting(column.getIsSorted() === "asc")}
-                >
-                    College Roll No
-                    <ArrowUpDown />
-                </Button>
-            )
-        },
-        cell: ({ row }) => <div className="ml-7 lowercase">{row.getValue("collegeRollNo")}</div>,
-    },
-    {
-        accessorKey: "universityRollNo",
-        header: ({ column }) => {
-            return (
-                <Button
-                    variant="ghost"
-                    onClick={() => column.toggleSorting(column.getIsSorted() === "asc")}
-                >
-                    University Roll No
-                    <ArrowUpDown />
-                </Button>
-            )
-        },
-        cell: ({ row }) => <div className="ml-7 lowercase">{row.getValue("universityRollNo")}</div>,
-    },
-    {
-        accessorKey: "name",
-        header: "Name",
-        cell: ({ row }) => (
-            <div className="capitalize">{row.getValue("name")}</div>
-        ),
-    },
-    {
-        accessorKey: "session",
-        header: ({ column }) => {
-            return (
-                <Button
-                    variant="ghost"
-                    onClick={() => column.toggleSorting(column.getIsSorted() === "asc")}
-                >
-                    Session
-                    <ArrowUpDown />
-                </Button>
-            )
-        },
-        cell: ({ row }) => <div className="ml-4 lowercase">{row.getValue("session")}</div>,
-    },
-    {
-        accessorKey: "currentSemester",
-        header: ({ column }) => {
-            return (
-                <Button
-                    variant="ghost"
-                    onClick={() => column.toggleSorting(column.getIsSorted() === "asc")}
-                >
-                    Current Semester
-                    <ArrowUpDown />
-                </Button>
-            )
-        },
-        cell: ({ row }) => <div className="ml-11 lowercase">{row.getValue("currentSemester")}</div>,
-    },
-    {
-        accessorKey: "attendance",
-        header: ({ column }) => {
-            return (
-                <Button
-                    variant="ghost"
-                    onClick={() => column.toggleSorting(column.getIsSorted() === "asc")}
-                >
-                    Attendence
-                    <ArrowUpDown />
-                </Button>
-            )
-        },
-        cell: ({ row }) => <div className="ml-11 lowercase">{row.getValue("attendance")}</div>,
-    },
-    {
-        accessorKey: "email",
-        header: ({ column }) => {
-            return (
-                <Button
-                    variant="ghost"
-                    onClick={() => column.toggleSorting(column.getIsSorted() === "asc")}
-                >
-                    Email
-                    <ArrowUpDown />
-                </Button>
-            )
-        },
-        cell: ({ row }) => <div className="lowercase">{row.getValue("email")}</div>,
-    },
-    {
-        accessorKey: "phoneNumber",
-        header: "Phone Number",
-        cell: ({ row }) => {
-            return <div>{row.getValue("phoneNumber")}</div>
-        },
-    },
-    {
-        id: "actions",
-        enableHiding: false,
-        cell: ({ row }) => {
-            const student = row.original
-
-            return (
-                <DropdownMenu>
-                    <DropdownMenuTrigger asChild>
-                        <Button variant="ghost" className="h-8 w-8 p-0">
-                            <span className="sr-only">Open menu</span>
-                            <MoreHorizontal />
-                        </Button>
-                    </DropdownMenuTrigger>
-                    <DropdownMenuContent align="end">
-                        <DropdownMenuLabel>Actions</DropdownMenuLabel>
-                        <DropdownMenuItem
-                            onClick={() => navigator.clipboard.writeText(student.id.toString())}
-                        >
-                            Copy Student ID
-                        </DropdownMenuItem>
-                        <DropdownMenuSeparator />
-                        <DropdownMenuItem>View Student</DropdownMenuItem>
-                        <DropdownMenuItem>View Student details</DropdownMenuItem>
-                    </DropdownMenuContent>
-                </DropdownMenu>
-            )
-        },
-    },
-]
+import { UpdateStudent } from "./update-student"
 
 export default function Students() {
     const [data, setData] = useState<IStudent[]>([]);
@@ -186,6 +28,189 @@ export default function Students() {
     const [columnVisibility, setColumnVisibility] =
         React.useState<VisibilityState>({})
     const [rowSelection, setRowSelection] = React.useState({})
+    const updateStudent = () => {
+        const students = JSON.parse(window.localStorage.getItem('students') || '[]') || [];
+        setData(students);
+    };
+
+    const columns: ColumnDef<IStudent>[] = [
+        {
+            id: "select",
+            header: ({ table }) => (
+                <Checkbox
+                    checked={
+                        table.getIsAllPageRowsSelected() ||
+                        (table.getIsSomePageRowsSelected() && "indeterminate")
+                    }
+                    onCheckedChange={(value) => table.toggleAllPageRowsSelected(!!value)}
+                    aria-label="Select all"
+                />
+            ),
+            cell: ({ row }) => (
+                <Checkbox
+                    checked={row.getIsSelected()}
+                    onCheckedChange={(value) => row.toggleSelected(!!value)}
+                    aria-label="Select row"
+                />
+            ),
+            enableSorting: false,
+            enableHiding: false,
+        },
+        {
+            accessorKey: "collegeRollNo",
+            header: ({ column }) => {
+                return (
+                    <Button
+                        variant="ghost"
+                        onClick={() => column.toggleSorting(column.getIsSorted() === "asc")}
+                    >
+                        College Roll No
+                        <ArrowUpDown />
+                    </Button>
+                )
+            },
+            cell: ({ row }) => <div className="ml-7 lowercase">{row.getValue("collegeRollNo")}</div>,
+        },
+        {
+            accessorKey: "universityRollNo",
+            header: ({ column }) => {
+                return (
+                    <Button
+                        variant="ghost"
+                        onClick={() => column.toggleSorting(column.getIsSorted() === "asc")}
+                    >
+                        University Roll No
+                        <ArrowUpDown />
+                    </Button>
+                )
+            },
+            cell: ({ row }) => <div className="ml-7 lowercase">{row.getValue("universityRollNo")}</div>,
+        },
+        {
+            accessorKey: "name",
+            header: "Name",
+            cell: ({ row }) => (
+                <div className="capitalize">{row.getValue("name")}</div>
+            ),
+        },
+        {
+            accessorKey: "session",
+            header: ({ column }) => {
+                return (
+                    <Button
+                        variant="ghost"
+                        onClick={() => column.toggleSorting(column.getIsSorted() === "asc")}
+                    >
+                        Session
+                        <ArrowUpDown />
+                    </Button>
+                )
+            },
+            cell: ({ row }) => <div className="ml-4 lowercase">{row.getValue("session")}</div>,
+        },
+        {
+            accessorKey: "currentSemester",
+            header: ({ column }) => {
+                return (
+                    <Button
+                        variant="ghost"
+                        onClick={() => column.toggleSorting(column.getIsSorted() === "asc")}
+                    >
+                        Current Semester
+                        <ArrowUpDown />
+                    </Button>
+                )
+            },
+            cell: ({ row }) => <div className="ml-11 lowercase">{row.getValue("currentSemester")}</div>,
+        },
+        {
+            accessorKey: "attendance",
+            header: ({ column }) => {
+                return (
+                    <Button
+                        variant="ghost"
+                        onClick={() => column.toggleSorting(column.getIsSorted() === "asc")}
+                    >
+                        Attendence
+                        <ArrowUpDown />
+                    </Button>
+                )
+            },
+            cell: ({ row }) => <div className="ml-11 lowercase">{row.getValue("attendance")}</div>,
+        },
+        {
+            accessorKey: "email",
+            header: ({ column }) => {
+                return (
+                    <Button
+                        variant="ghost"
+                        onClick={() => column.toggleSorting(column.getIsSorted() === "asc")}
+                    >
+                        Email
+                        <ArrowUpDown />
+                    </Button>
+                )
+            },
+            cell: ({ row }) => <div className="lowercase">{row.getValue("email")}</div>,
+        },
+        {
+            accessorKey: "phoneNumber",
+            header: "Phone Number",
+            cell: ({ row }) => {
+                return <div>{row.getValue("phoneNumber")}</div>
+            },
+        },
+        {
+            id: "actions",
+            enableHiding: false,
+            cell: ({ row }) => {
+                const student = row.original
+                const [open, setOpen] = React.useState(false);
+    
+                const handleCloseDialog = () => {      
+                    updateStudent();         
+                    setOpen(false);
+                    toast.success(`${student.name} updated successfully!`);
+                }
+                return (
+                    <>
+                        <DropdownMenu>
+                            <DropdownMenuTrigger asChild>
+                                <Button variant="ghost" className="h-8 w-8 p-0">
+                                    <span className="sr-only">Open menu</span>
+                                    <MoreHorizontal />
+                                </Button>
+                            </DropdownMenuTrigger>
+                            <DropdownMenuContent align="end">
+                                <DropdownMenuLabel>Actions</DropdownMenuLabel>
+                                <DropdownMenuItem
+                                    onClick={() => navigator.clipboard.writeText(student.id.toString())}
+                                >
+                                    Copy Student ID
+                                </DropdownMenuItem>
+                                <DropdownMenuSeparator />
+                                <DropdownMenuItem onClick={() => setOpen(true)} className="text-gray-600"><SquarePen /> Edit</DropdownMenuItem>
+                                <DropdownMenuItem className="text-red-500"><Trash2 /> Delete</DropdownMenuItem>
+                            </DropdownMenuContent>
+                        </DropdownMenu>
+                        {/* Update Student Data Dialog */}
+                        <Dialog open={open} onOpenChange={setOpen}>
+                            <DialogContent className="lg:max-w-[40vw] max-h-[65vh] overflow-y-auto p-6 rounded-lg shadow-lg">
+                                <DialogHeader>
+                                    <DialogTitle>Update Student</DialogTitle>
+                                    <DialogDescription>
+                                        Update student. Click submit when you're done.
+                                    </DialogDescription>
+                                </DialogHeader>
+                                <UpdateStudent studentId={student.id} onSave={handleCloseDialog} />
+                            </DialogContent>
+                        </Dialog>
+                    </>
+                )
+    
+            },
+        },
+    ]
 
     const table = useReactTable({
         data,
@@ -211,7 +236,7 @@ export default function Students() {
         setData(students);
         setOpen(false);
         var last = students[students.length - 1];
-        toast.success(`${last.name} added successfully`);
+        toast.success(`${last.name} added successfully!`);
     }
     const [open, setOpen] = React.useState(false);
     return (
@@ -220,11 +245,11 @@ export default function Students() {
                 <h1 className="text-2xl font-semibold">Students</h1>
                 <Button onClick={() => setOpen(true)} variant="outline">Add student</Button>
                 <Dialog open={open} onOpenChange={setOpen}>
-                    <DialogContent className="lg:max-w-screen-lg overflow-y-scroll max-h-screen">
+                    <DialogContent className="lg:max-w-[40vw] max-h-[65vh] overflow-y-auto p-6 rounded-lg shadow-lg">
                         <DialogHeader>
                             <DialogTitle>Add Student</DialogTitle>
                             <DialogDescription>
-                                Add student. Click save when you're done.
+                                Add student. Click submit when you're done.
                             </DialogDescription>
                         </DialogHeader>
                         <RegisterStudent onSave={handleCloseDialog} />

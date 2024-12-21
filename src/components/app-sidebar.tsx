61d93e051931cdd4b0ef5828765c93b7336781f6
Zubair Jamil <xubairjamil@gmail.com>
2024-12-21 07:33:05 +0500

    fix bugs

--- src/components/app-sidebar.tsx
+++ src/components/app-sidebar.tsx
@@ -20,7 +20,9 @@ import {
   SidebarMenuItem,
 } from "@/components/ui/sidebar"
 import Link from "next/link"
-import Image from "next/image"
+import Image from "next/image";
+import logo from "../../public/logo.png";
+
 
 const data = {
   navMain: [
@@ -82,7 +84,7 @@ export function AppSidebar({ ...props }: React.ComponentProps<typeof Sidebar> ) 
             <SidebarMenuButton size="lg" asChild>
               <Link href={"/"}>
                 <div className="flex aspect-square size-8 items-center justify-center rounded-lg  text-sidebar-primary-foreground">
-                  <Image className="size-8" src={"logo.png"} alt={"logo of  mark-me"}></Image>
+                  <Image className="size-8" src={logo} alt={"logo of mark-me"} width={80} height={80}></Image>
                 </div>
                 <div className="grid flex-1 text-left text-sm leading-tight">
                   <span className="truncate text-md font-semibold">Mark Me</span>
